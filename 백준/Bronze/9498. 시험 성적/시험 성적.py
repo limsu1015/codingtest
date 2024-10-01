f31b00ae3777278171c31333810c519f24145300
limsu1015 <tngur1015@gmail.com>
2024-10-01 12:20:47 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+n = int(input())
+if 90 <= n <= 100:
+    print('A')
+elif 80 <= n < 90:
+    print('B')
+elif 70 <= n < 80:
+    print('C')
+elif 60 <= n < 70:
+    print('D')
+else:
+    print('F')
