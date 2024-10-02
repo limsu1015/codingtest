adc62594012389a23f0a210d84bfc4b1d81529b0
limsu1015 <tngur1015@gmail.com>
2024-10-02 22:49:26 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,9 +1,7 @@
 a, b = map(int, input().split( ))
 c = int(input())
+cook = a*60 + b + c
+cook_hour = (cook//60) % 24
+cook_minute = cook % 60
 if 0 <= a <= 23 and 0 <= b <= 59 and 0 <= c <= 1000:
-    if b + c >= 60 and a+((b+c)//60) < 24:
-        print(f'{a+((b+c)//60)} {(b+c)%60}')
-    elif b + c >= 60 and a+((b+c)//60) >= 24:
-        print(f'{(a+((b+c)//60))-24} {(b+c)%60}')
-    else:
-        print(f'{a} {b+c}')
+    print(f'{cook_hour} {cook_minute}')
