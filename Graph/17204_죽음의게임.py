1ad7024c00fadaac30f10a13e28d5a963abd66f7
limsu1015 <tngur1015@gmail.com>
2024-11-09 23:59:09 +0900

    Add files via upload
    17204번 풀이방법 주석 추가

--- Graph/17204_죽음의게임.py
+++ Graph/17204_죽음의게임.py
@@ -8,14 +8,14 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 a = [int(input()) for _ in range(n)]
-turn = 0
-count = 0
+turn = 0 # 0번부터  지목 시작
+count = 0 # 지목 횟수
 for i in range(n):
-    target = a[turn]
+    target = a[turn] # 첫 턴은 무조건 0번 부터 지목한 사람 (a의 0번 인덱스 시작)
     count += 1
     if target == k:
         print(count)
         break
-    turn = target
+    turn = target # a[0]의 인덱스 1로 위에 target = a[1]로 돌아가서 if문 만족할때까지 반복
 else:
     print(-1)
